b13b4695ce612732726a74fb8cb932d43f3d0701
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-02-08 20:26:09 -0800

    had to create a new repo, problems with the last one

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Western Bacon Cheeseburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Hawaiian', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Kilauea', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bourbon', false);
