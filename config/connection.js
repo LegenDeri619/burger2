791e51b5af79a6e6b2a13053ef0125bb5c51a6a8
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-02-08 21:50:14 -0800

    connections changed

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,24 @@
-// Set up MySQL connection.
+// Connect Node to MySQL.
 var mysql = require("mysql");
 
-//heroku jawsDB connection
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
-connection.end();
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burger_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// Export connection for our ORM to use.
+// Export the connection.
 module.exports = connection;
